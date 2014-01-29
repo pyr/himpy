3e306df613e7d24070e2c8aab24ca98c39c88bdb
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-01-29 21:38:57 +0100

    handle connect failures

--- Himpy/Mib.hs
+++ Himpy/Mib.hs
@@ -1,62 +1,36 @@
 module Himpy.Mib where
-import qualified Network.Protocol.NetSNMP as S
+import Network.Protocol.NetSNMP (RawOID)
 
-ifDescr :: S.RawOID
-ifDescr = [1, 3, 6, 1, 2, 1, 2, 2, 1, 2]
-
-ifName :: S.RawOID
-ifName = [1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 1 ]
-
-ifInOctets :: S.RawOID
-ifInOctets = [1, 3, 6, 1, 2, 1, 2, 2, 1, 10]
-
-ifOutOctets :: S.RawOID
-ifOutOctets = [1, 3, 6, 1, 2, 1, 2, 2, 1, 16]
-
-ifOperStatus :: S.RawOID
-ifOperStatus = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8]
-
-ifConnectorPresent :: S.RawOID
-ifConnectorPresent = [1, 3, 6, 1, 2, 1, 2, 2, 1, 17]
-
-ifAdminStatus :: S.RawOID
-ifAdminStatus = [1, 3, 6, 1, 2, 1, 2, 2, 1, 7]
-
-sysName :: S.RawOID
-sysName = [1, 3, 6, 1, 2, 1, 1, 5, 0]
-
-hrStorageDescr :: S.RawOID
-hrStorageDescr = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 3]
-
-hrStorageSize :: S.RawOID
-hrStorageSize = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 5]
-
-hrStorageUsed :: S.RawOID
-hrStorageUsed = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 6]
-
-hrStorageAllocationUnits :: S.RawOID
-hrStorageAllocationUnits = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 4]
-
-lanMgrServiceDescr :: S.RawOID
-lanMgrServiceDescr = [1, 3, 6, 1, 4, 1, 77, 1, 2, 3, 1, 1]
-
-lanMgrServiceStatus :: S.RawOID
-lanMgrServiceStatus = [1, 3, 6, 1, 4, 1, 77, 1, 2, 3, 1, 3]
-
-hrProcessorLoad :: S.RawOID
-hrProcessorLoad = [1, 3, 6, 1, 2, 1, 25, 3, 3, 1, 2]
-
-pfeCpuPercent :: S.RawOID
-pfeCpuPercent = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,4,0]
-
-pfeSessions :: S.RawOID
-pfeSessions = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,6,0]
-
-pfeSessionMax :: S.RawOID
-pfeSessionMax = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,7,0]
-
-pfePSU1 :: S.RawOID
-pfePSU1 = [1,3,6,1,4,1,2636,3,1,15,1,8,2,1,0,0]
-
-pfePSU2 :: S.RawOID
-pfePSU2 = [1,3,6,1,4,1,2636,3,1,15,1,8,2,2,0,0]
+ifDescr = [1, 3, 6, 1, 2, 1, 2, 2, 1, 2] :: RawOID
+ifName = [1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 1 ] :: RawOID
+ifInOctets = [1, 3, 6, 1, 2, 1, 2, 2, 1, 10] :: RawOID
+ifOutOctets = [1, 3, 6, 1, 2, 1, 2, 2, 1, 16] :: RawOID
+ifOperStatus = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8] :: RawOID
+ifConnectorPresent = [1, 3, 6, 1, 2, 1, 2, 2, 1, 17] :: RawOID
+ifAdminStatus = [1, 3, 6, 1, 2, 1, 2, 2, 1, 7] :: RawOID
+sysName = [1, 3, 6, 1, 2, 1, 1, 5, 0] :: RawOID
+hrStorageDescr = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 3] :: RawOID
+hrStorageSize = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 5] :: RawOID
+hrStorageUsed = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 6] :: RawOID
+hrStorageAllocationUnits = [1, 3, 6, 1, 2, 1, 25, 2, 3, 1, 4] :: RawOID
+lanMgrServiceDescr = [1, 3, 6, 1, 4, 1, 77, 1, 2, 3, 1, 1] :: RawOID
+lanMgrServiceStatus = [1, 3, 6, 1, 4, 1, 77, 1, 2, 3, 1, 3] :: RawOID
+hrProcessorLoad = [1, 3, 6, 1, 2, 1, 25, 3, 3, 1, 2] :: RawOID
+pfeCpuPercent = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,4,0] :: RawOID
+pfeSessions = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,6,0] :: RawOID
+pfeSessionMax = [1,3,6,1,4,1,2636,3,39,1,12,1,1,1,7,0] :: RawOID
+pfePSU1 = [1,3,6,1,4,1,2636,3,1,15,1,8,2,1,0,0] :: RawOID
+pfePSU2 = [1,3,6,1,4,1,2636,3,1,15,1,8,2,2,0,0] :: RawOID
+hrSystemProcesses = [1,3,6,1,2,1,25,1,6,0] :: RawOID
+ciscoCpuRouting = [1,3,6,1,4,1,9,9,109,1,1,1,1,8,1] :: RawOID
+ciscoCpuSwitching = [1,3,6,1,4,1,9,9,109,1,1,1,1,8,2] :: RawOID
+ciscoMemAvail = [1,3,6,1,4,1,9,9,48,1,1,1,6,1] :: RawOID
+ciscoMemUsed = [1,3,6,1,4,1,9,9,48,1,1,1,5,1] :: RawOID
+ciscoSwitchCpu = [1,3,6,1,4,1,9,9,109,1,1,1,1,7,1] :: RawOID
+ciscoSwitchMem = [1,3,6,1,4,1,9,9,305,1,1,2,0] :: RawOID
+ciscoRouterPSU1 = [1,3,6,1,4,1,9,9,13,1,5,1,3,1] :: RawOID
+ciscoRouterPSU2 = [1,3,6,1,4,1,9,9,13,1,5,1,3,2] :: RawOID
+ciscoSwitchPSU1 = [1,3,6,1,4,1,9,9,117,1,1,2,1,2,470] :: RawOID
+ciscoSwitchPSU2 = [1,3,6,1,4,1,9,9,117,1,1,2,1,2,471] :: RawOID
+zfsPoolDescr = [1,3,6,1,4,1,25359,1,1,1] :: RawOID
+zfsPoolHealth = [1,3,6,1,4,1,25359,1,1,4] :: RawOID
--- Himpy/Output/Riemann.hs
+++ Himpy/Output/Riemann.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Himpy.Output.Riemann (riemann_start, riemann_send) where
 import Himpy.Types
 import Himpy.Utils
@@ -19,26 +20,32 @@ import Control.Monad.STM (atomically)
 import Control.Concurrent.STM.TChan (writeTChan, readTChan, newTChanIO, TChan)
 import System.IO
 import Data.Word (Word32)
+import Control.Exception
 import qualified Data.ByteString as B
 
 -- Simplistic riemann write module
 
-riemann_write :: TChan String -> TChan [Metric] -> Handle -> IO ()
-riemann_write logchan chan fd = do
+riemann_write_out fd hmsg = do
+  B.hPut fd hmsg
+  hFlush fd
+
+riemann_write :: TChan String -> TChan [Metric] -> String -> IO ()
+riemann_write logchan chan host = do
   metrics <- atomically $ readTChan chan
   msg <- metrics_to_msg metrics
   let hdr = B.pack $ octets $ (fromIntegral (B.length msg) :: Word32)
   let hmsg = B.concat [hdr, msg]
   log_info logchan $ "sending out: " ++ (show $ length metrics)
-  B.hPut fd hmsg
-  hFlush fd
+
+  fd <- connectTo host (PortNumber 5555)
+  let handler = (\(e :: SomeException) -> log_info logchan $ "send error: " ++ (show e))
+  riemann_write_out fd hmsg `catch` handler `finally` hClose fd
   return ()
 
 riemann_start :: TChan String -> String -> IO (TChan [Metric])
-riemann_start logchan host = withSocketsDo $ do
+riemann_start logchan host = do
   chan <- newTChanIO
-  fd <- connectTo host (PortNumber 5555)
-  void $ forkIO $ forever $ riemann_write logchan chan fd
+  void $ forkIO $ forever $ riemann_write logchan chan host
   return (chan)
 
 riemann_send :: TChan [Metric] -> [Metric] -> IO ()
